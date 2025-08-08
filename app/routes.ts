ae8d344f61d425b314821f869a0311fbff4c2d60
michaelmephi <michaelmephi@gmail.com>
2025-08-08 05:03:53 +0200

    Resume Feed Page

--- app/routes.ts
+++ app/routes.ts
@@ -1,4 +1,4 @@
 import { type RouteConfig, index , route} from "@react-router/dev/routes";
 
 export default [index("routes/home.tsx"),
-    route ('/auth','routes/auth.tsx'),route('/upload','routes/upload.tsx')]satisfies RouteConfig;
+    route ('/auth','routes/auth.tsx'),route('/upload','routes/upload.tsx'),route('/resume/:id','routes/resume.tsx'), ]satisfies RouteConfig;
--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -57,6 +57,7 @@ const Upload = () => {
         await kv.set(`resume:${uuid}`, JSON.stringify(data));
         setStatusText('analysis complete, redirecting....');
         console.log(data);
+        navigate(`/resume/${uuid}`);
     }
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
